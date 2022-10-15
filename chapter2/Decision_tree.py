d029f06c91a1e49198a47e6a0623bd596505a0c7
arslanlahori <arslanansari786.iu@gmail.com>
2022-10-15 17:46:39 +0500

    move files to folder chapter2

---
+++ chapter2/Decision_tree.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from sklearn import preprocessing
+import mglearn
+from sklearn.datasets import load_breast_cancer
+from sklearn.tree import DecisionTreeClassifier
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+
+tree = DecisionTreeClassifier(random_state=0)
+tree.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+
+print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
+
+tree = DecisionTreeClassifier(max_depth=4, random_state=0)
+tree.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
---
+++ chapter2/Ridge.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from sklearn import preprocessing
+import mglearn
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+X, y = mglearn.datasets.load_extended_boston()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+ridge = Ridge()
+ridge.fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))
+
+
+ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge10.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge10.score(X_test, y_test)))
+
+ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(ridge01.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge01.score(X_test, y_test)))
+
+
+plt.plot(ridge.coef_, 's', label="Ridge alpha=1")
+plt.plot(ridge10.coef_, '^', label="Ridge alpha=10")
+plt.plot(ridge01.coef_, 'v', label="Ridge alpha=0.1")
+plt.plot(lr.coef_, 'o', label="LinearRegression")
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.hlines(0, 0, len(lr.coef_))
+plt.ylim(-25, 25)
+plt.legend()
